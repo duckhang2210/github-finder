c4fa4a1cf19a0757941f6a39edcd820023508069
unknown <duckhang2210@gmail.com>
2021-10-22 15:45:17 -0500

    add navbar and users

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<Navbar title='Github Finder' icon='fab fa-github' />
+				<div className='container'>
+					<Users />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/users/UsersItems.jsx
@@ -0,0 +1,15 @@
+const UsersItems = ({ user: {login, avatar_url, html_url}}) => {    
+    
+    return (
+        <div className='card text-center'>
+            <img src={avatar_url} alt="" className='round-img'style={{width: '60px'}}/>
+            <h3>{login}</h3>
+            <div>
+                <a target='_blank' rel='noreferrer' href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+            </div>
+        </div>
+    )
+    
+}
+
+export default UsersItems
